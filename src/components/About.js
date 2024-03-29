8a50916a06104b1e14156e05b28f50dc3b92fcd3
unknown <afyaakhudur@gmail.com>
2024-03-29 05:49:06 +0300

    complete assynment

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (<div id="about">
+    <h1>About Me</h1>
+    <p>hey I am a developer</p>
+    <img src={image} alt="I made this"/>
+  </div>);
 }
 
 export default About;
